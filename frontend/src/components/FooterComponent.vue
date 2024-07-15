def13c7e5114398a6f0b74976ea8f36b9ad49070
Alex Gonzalez <galex2003@icloud.com>
2024-07-15 14:33:12 -0400

    Adding a backend to the proj

---
+++ frontend/src/components/FooterComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-footer class="ax-0" color="indigo-lighten-2">
+    <v-row justify="center" no-gutters>
+      <v-btn
+        v-for="link in links"
+        :key="link.link"
+        class="mx-2"
+        :href="link.link"
+        color="white"
+        rounded="xl"
+        variant="text"
+        :prepend-icon="link.icon"
+      >
+        {{ link.name }}
+      </v-btn>
+      <v-col class="text-center mt-4" cols="12">
+        {{ new Date().getFullYear() }} — <strong>Alex Gonzalez</strong
+        ><a
+          href="https://www.google.com/maps/place/Miami,+FL"
+          style="text-decoration: none; color: inherit"
+          class="mx-2"
+          target="_blank"
+          ><v-icon icon="mdi-map-marker" />Miami, FL</a
+        >
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script setup lang="ts">
+import { SocialLink } from '@/types/SocialLink'
+const links: SocialLink[] = [
+  {
+    link: 'https://github.com/alex-305',
+    name: 'github',
+    icon: 'fa:fa-brands fa-github'
+  },
+  {
+    link: 'https://alex305.itch.io',
+    name: 'itch.io',
+    icon: 'fa:fa-brands fa-itch-io'
+  },
+  {
+    link: 'https://x.com/itsslax',
+    name: 'x',
+    icon: 'fa:fa-brands fa-x-twitter'
+  }
+]
+</script>
