75baaedf202f0f2081bdd133b54266566568633d
Alex Gonzalez <galex2003@icloud.com>
2024-07-16 14:42:23 -0400

    updated nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,22 +1,34 @@
 <template>
   <v-container>
-    <v-card variant="text" class="d-flex flex-md-row flex-column justify-center">
+    <v-card variant="text" class="d-flex flex-row justify-center">
       <NavBtn name="portfolio" icon="mdi-projector-screen">Projects</NavBtn>
       <v-hover>
-        <v-card variant="tonal" class="d-flex justify-center align-center" color="primary">
-          <div class="cursor-pointer text-h2 ma-3" @click="ToHome">Alex Gonzalez</div>
+        <v-card @click="ToHome" variant="tonal" class="d-flex justify-center align-center" color="primary">
+          <div v-if="!isMobile" class="cursor-pointer text-h2 ma-3">Alex Gonzalez</div>
+          <div v-else class="cursor-pointer text-h4 ma-3">Alex</div>
         </v-card>
       </v-hover>
       <NavBtn name="blog" icon="mdi-post-outline">Blog</NavBtn>
     </v-card>
+    <v-divider class="my-3"></v-divider>
   </v-container>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
+import { computed } from 'vue';
 import NavBtn from '@/components/NavBtn.vue'
 
 const router = useRouter()
+
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay()
+
+const isMobile = computed((): boolean => {
+  return mobile.value
+})
+
 
 function ToHome() {
   router.push({ name: 'home' })
--- src/components/NavBtn.vue
+++ src/components/NavBtn.vue
@@ -4,15 +4,25 @@
     variant="tonal"
     :to="{ name: props.name }"
     class="mx-3 my-auto"
-    :prepend-icon="props.icon"
   >
-    <slot></slot>
+  <v-icon :icon="props.icon"/>
+    <slot v-if="!isMobile"></slot>
   </v-btn>
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import { useDisplay } from 'vuetify'
+
+const { mobile } = useDisplay()
+
+const isMobile = computed((): boolean => {
+  return mobile.value
+})
+
 const props = defineProps<{
   name: string
   icon: string
 }>()
+
 </script>
