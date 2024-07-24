513689e0af9411ac938246a8bc774c5b3a36baff
Alex Gonzalez <galex2003@icloud.com>
2024-07-24 00:09:36 -0400

    Made navbar more satisfying and more user feedback

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container class="mb-5">
     <v-card variant="text" class="d-flex flex-row justify-center">
-      <NavBtn route="/projects" icon="mdi-projector-screen">Projects</NavBtn>
-      <NavBtn route="/"><span v-if="isWideEnough">Alex Gonzalez</span><span v-else>Alex</span></NavBtn>
-      <NavBtn route="/blog" icon="mdi-post-outline">Blog</NavBtn>
+      <NavBtn route="/projects" :active="isActive('/projects')" icon="mdi-projector-screen">Projects</NavBtn>
+      <NavBtn route="/" :active="isActive('/')"><span v-if="isWideEnough">Alex Gonzalez</span><span v-else>Alex</span></NavBtn>
+      <NavBtn route="/blog" :active="isActive('/blog')" icon="mdi-post-outline">Blog</NavBtn>
     </v-card>
   </v-container>
 </template>
@@ -13,6 +13,10 @@ import NavBtn from './NavBtn.vue';
 import { useDisplay } from 'vuetify'
 
 const { width } = useDisplay()
+
+const route = useRoute()
+
+const isActive = (path:string) => { return path === route.path }
 
 const isWideEnough = computed(()=> { return width.value > 400})
 </script>
--- components/NavBtn.vue
+++ components/NavBtn.vue
@@ -4,6 +4,8 @@
     variant="tonal"
     :to="props.route"
     class="mx-3 my-auto"
+    :active="props.active"
+    :class="{ bigger: props.active }"
   >
   <v-icon v-if="props.icon!=''" :icon="props.icon"/>
     <slot v-if="isWideEnough || props.icon ==''"></slot>
@@ -20,8 +22,21 @@ const isWideEnough = computed(() => { return width.value >= 500})
 const props = withDefaults(defineProps<{
   route: string
   icon?: string
+  active: boolean
 }>(), {
   icon:''
 })
 
 </script>
+
+<style scoped>
+
+.v-btn {
+  transition: all 0.2s ease;
+}
+
+.bigger {
+  height: 50px;
+  font-size: 20px;
+}
+</style>
