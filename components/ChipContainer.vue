a7938fbc51785ab250968ba584637f3a763e9689
Alex Gonzalez <galex2003@icloud.com>
2024-08-06 10:39:54 -0400

    Fixed issue with removing chips

--- components/ChipContainer.vue
+++ components/ChipContainer.vue
@@ -7,7 +7,7 @@
         color="tertiary"
         :class="['ma-auto', 'mx-1', props.chipClass]"
         @click="emits('select', chip, index)"
-        @click:close="emits('remove', chip, index)"> {{ chip }}</v-chip>
+        @click:close.prevent="emits('remove', chip, index)"> {{ chip }}</v-chip>
     </v-card>
 </template>
 
