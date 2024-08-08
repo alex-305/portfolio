52c856a9347de92a7f40079ff63c29fba7202409
Alex Gonzalez <galex2003@icloud.com>
2024-08-08 11:53:01 -0400

    Finally fixed double chip remove bug

--- components/ChipContainer.vue
+++ components/ChipContainer.vue
@@ -2,13 +2,15 @@
     <v-card :variant="props.variant" class="d-inline-flex">
         <v-chip
         v-for="(chip, index) in props.chips" 
-        :closable="removable" 
         :link="selectable" 
         :color="color"
         :prepend-icon="chipIcon"
         :class="['ma-auto', 'mx-1', props.chipClass]"
-        @click="emits('select', chip, index)"
-        @click:close.stop="emits('remove', chip, index)"> {{ chip }}</v-chip>
+        @click="emits('select', chip, index)"> {{ chip }}
+            <template v-if="removable" #close>
+                <v-icon icon="mdi-close-circle" @click.stop="emits('remove', chip, index)" />
+            </template>
+        </v-chip>
     </v-card>
 </template>
 
