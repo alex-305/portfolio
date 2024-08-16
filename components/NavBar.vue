7ede5517118f539abddc4189701070356a2f3ee2
Alex Gonzalez <galex2003@icloud.com>
2024-08-16 15:31:20 -0400

    Added tooltip to theme switcher

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -7,13 +7,19 @@
           <span v-if="isWideEnough">Alex Gonzalez</span>
           <span v-else>Alex</span></NavBtn>
         <NavBtn route="/blog" :active="isActive('/blog')" icon="mdi-post-outline">Blog</NavBtn>
-        <div v-if="btnWideEnough" class="position-absolute right-0 pa-1">
-          <v-btn @click="toggleTheme(theme)" variant="text" class="right-0 pa-0 mr-2" style="font-size: 20px; width: 38px; height: 38px;" icon="mdi-shield-sun"/>
+        <div class="position-absolute right-0 pa-1">
+          <v-tooltip text="Toggle theme">
+            <template v-slot:activator="{props}">
+              <v-fab
+                v-bind="props"
+                @click="toggleTheme(theme)" 
+                variant="text" class="right-0 pa-0 mr-2" 
+                style="font-size: 20px; width: 38px; height: 38px;" 
+                icon="mdi-shield-sun"/>
+            </template>
+          </v-tooltip>
         </div>
       </v-card>
-      <div v-if="!btnWideEnough">
-        <v-fab location="bottom right" class="pa-0 mr-2" style="font-size: 20px; width: 38px; height: 38px;" icon="mdi-shield-sun"/>
-      </div>
     </v-container>
   </ClientOnly>
 </template>
@@ -31,9 +37,7 @@ const route = useRoute()
 
 const isActive = (path:string) => { return path === route.path || path==='/blog' && route.path.includes('/blog') }
 
-const btnWideEnough = computed(() => { return width.value > 700})
-
-const isWideEnough = computed(()=> { return width.value > 460})
+const isWideEnough = computed(()=> { return width.value > 600})
 </script>
 
 <style scoped></style>
