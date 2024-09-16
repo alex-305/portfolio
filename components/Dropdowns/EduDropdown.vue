6a0815bed24360d72287f74597a69571c41ab4ff
Alex Gonzalez <galex2003@icloud.com>
2024-09-16 11:00:45 -0400

    Made education appear from highest achieved to lowest

--- components/Dropdowns/EduDropdown.vue
+++ components/Dropdowns/EduDropdown.vue
@@ -1,5 +1,17 @@
 <template>
   <DropDownComponent title="Education" icon="mdi-account-school-outline">
+    <div class="d-flex my-3 justify-space-between">
+      <span class="text-h4">Florida State University</span>
+      <span class="text-h5 ml-auto"> Summer 2021 - Fall 2024</span>
+    </div>
+    <div class="text-body-1">
+      The Florida State University experience is unlike any other. I have never met a group of
+      people so dedicated to both their schoolwork and partying at the same time. While, I've had
+      my fair share of fun at this school, my biggest takeways have been what I have learned in
+      the classroom. I came into Florida State with no direction and certainly no idea what I
+      wanted my professional life to look like. Through my advisors and my experiences at the
+      school, I came to realize that I had a passion for computers. This has led me down the path that I have been on for almost 2 years of an obsession with programming, computers, and technology as a whole. 
+    </div>
     <div class="d-flex flex-column">
       <div class="d-flex my-3 justify-space-between">
         <span class="text-h4">Christopher Columbus High School</span>
@@ -11,18 +23,6 @@
         in me. The school fostered a brotherhood unlike anywhere else I've been. I met some of my
         closest friends while attending Christopher Columbus and some of my best memories are from
         my time on campus. I am grateful to have had the opportunity to attend the school.
-      </div>
-      <div class="d-flex my-3 justify-space-between">
-        <span class="text-h4">Florida State University</span>
-        <span class="text-h5 ml-auto"> Summer 2021 - Fall 2024</span>
-      </div>
-      <div class="text-body-1">
-        The Florida State University experience is unlike any other. I have never met a group of
-        people so dedicated to both their schoolwork and partying at the same time. While, I've had
-        my fair share of fun at this school, my biggest takeways have been what I have learned in
-        the classroom. I came into Florida State with no direction and certainly no idea what I
-        wanted my professional life to look like. Through my advisors and my experiences at the
-        school, I came to realize that I had a passion for computers. This has led me down the path that I have been on for almost 2 years of an obsession with programming, computers, and technology as a whole. 
       </div>
     </div>
   </DropDownComponent>
